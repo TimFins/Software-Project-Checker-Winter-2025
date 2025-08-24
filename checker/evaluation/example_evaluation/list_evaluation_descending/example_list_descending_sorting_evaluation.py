530d6308485383c873e1dd71378a3a4d72bf7e0e
timfinmans <tim@finmans.de>
2025-08-24 15:42:44 +0200

    Some comments were added to the evaluation part.

--- checker/evaluation/example_evaluation/list_evaluation_descending/example_list_descending_sorting_evaluation.py
+++ checker/evaluation/example_evaluation/list_evaluation_descending/example_list_descending_sorting_evaluation.py
@@ -6,34 +6,43 @@ from ..example_evaluation_utilities.example_evaluation_values_utilities import *
 def example_list_descending_sorting_evaluation(student_list: ExampleList, provided_list: ExampleList) -> tuple[int, str, ExampleList]:
     solution = provided_list.sorted(sort_ascending=False)
 
+    ### 1. Just one element to be sorted so the solution is trivial.
     if len(provided_list.get_data()) < 2:
         return 100, "Trivial solution.", solution
 
+    ### 2. No submission exists.
     if not student_list.get_data():
         return 0, "Empty submission.", solution
 
+    ### 3. The programatically created solution exactly equals the submission.
     if student_list.get_data() == solution.get_data():
         return 100, "Correct.", solution
 
+    ### 4. The submission is sorted ascending and not descending as expected.
     if student_list.get_data() == provided_list.sorted(sort_ascending=True).get_data():
         return 45, "You were supposed to sort in descending order but sorted in ascending order.", solution
 
+    ### Determine additional mistakes by using functions.
     sorting_violations = get_descending_violations(student_list)
     missing_values = get_missing_values(student_list, provided_list)
     additional_values = get_additional_values(student_list, provided_list)
 
     feedback = ""
     score = 100
-
+    
+    ### 5. Check if sorting violations exist and print them if there are any. Sorting violations mean
+    ###    a value is not sorted into the correct place.
     if sorting_violations:
         feedback += "The list is not correctly sorted in descending order. " + \
             "".join(sorting_violations)
         score -= len(sorting_violations) * 40
 
+    ### 6. Check if there are values missing which should be present and print them if there are any.
     if missing_values:
         feedback += f"The following values are present less often than expected: {missing_values}. "
         score -= len(missing_values) * 20
 
+    ### 7. Check if there are additional values which should not be present and print them if there are any.
     if additional_values:
         feedback += f"The following values are present more often than expected: {additional_values}. "
         score -= len(additional_values) * 20
