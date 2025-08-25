3eed72c87ad924a08fdc8eccf5b39e3a4afccd80
Edward Späth <edward5000@gmx.de>
2025-08-25 13:52:35 +0200

    Updated example_usage files to avoid problems with autoformatting

--- checker/example_usage_avl.py
+++ checker/example_usage_avl.py
@@ -5,22 +5,19 @@ To avoid the output from being overwhelming, after each console output you have 
 For that purpose the '_wait_for_user_confirmation()' function is used here. Please ignore it.
 """
 
-def _wait_for_user_confirmation(output_text):
-    input(
-        f"Please press 'Enter' to continue to continue past output {output_text}")
-
-##############
-# 1. Imports #
-##############
-
 # Import the AVLTreeNode class
 from datastructures.avltree import AVLTreeNode
 # Import the json module for parsing the JSON string into a dictionary
 import json
 
 
+def _wait_for_user_confirmation(output_text):
+    input(
+        f"Please press 'Enter' to continue to continue past output {output_text}")
+
+
 ##############################
-# 2. Creating an AVLTreeNode #
+# 1. Creating an AVLTreeNode #
 ##############################
 
 # This is an example for an input JSON, where there is just one node (hence balancing factor 0).
@@ -35,12 +32,12 @@ root = AVLTreeNode.from_dict(example_dictionary)
 
 # If Graphviz is installed, the AVL tree can be visualized by calling 'display_avl_image(title)'
 root.display_avl_image(
-    "2.1 This is the initial AVL tree\ncontaining just '5' with balance value '0'.")
-_wait_for_user_confirmation("2.1")
+    "1.1 This is the initial AVL tree\ncontaining just '5' with balance value '0'.")
+_wait_for_user_confirmation("1.1")
 
 
 #############################
-# 3. Modifying the AVL tree #
+# 2. Modifying the AVL tree #
 #############################
 
 # If you want to add a node to the tree, create a new node and add it as a child of the root.
@@ -54,8 +51,8 @@ root.set_right_child(new_right_child)
 root.set_balance(-1)
 
 root.display_avl_image(
-    "3.1 Now a right child was added.\nThe root's balance was adjusted accordingly.")
-_wait_for_user_confirmation("3.1")
+    "2.1 Now a right child was added.\nThe root's balance was adjusted accordingly.")
+_wait_for_user_confirmation("2.1")
 
 # Now two nodes are added at the correct positions.
 # One to the left of the root and one to the left of the root's right child.
@@ -70,12 +67,12 @@ root.get_right_child().set_left_child(new_right_left_child)
 root.get_right_child().set_balance(1)
 
 root.display_avl_image(
-    "3.2 Two nodes were added and the balance values were adjusted accordingly.")
-_wait_for_user_confirmation("3.2")
+    "2.2 Two nodes were added and the balance values were adjusted accordingly.")
+_wait_for_user_confirmation("2.2")
 
 
 #################################################
-# 4. Creating a copy and comparing for equality #
+# 3. Creating a copy and comparing for equality #
 #################################################
 
 # Now create a copy, so it can be modified without affecting the original. Use the deep_copy() method for that.
@@ -85,26 +82,26 @@ root_copy = root.deep_copy()
 root_copy.get_right_child().set_value(8)
 
 # One can use the regular comparison operators to check if two nodes are equal (have the same value and balance value)
-print("4.1 root.get_right_child() == root_copy.get_right_child() =",
+print("3.1 root.get_right_child() == root_copy.get_right_child() =",
       root.get_right_child() == root_copy.get_right_child())
 # The result is: False. The node was modified and is therefore not equal to the original.
-_wait_for_user_confirmation("4.1")
+_wait_for_user_confirmation("3.1")
 
-print("4.2 root == root_copy =", root == root_copy)
+print("3.2 root == root_copy =", root == root_copy)
 # The result is: True. While the right child of the root was modified, the root itself was not changed.
 # So the roots are still equal.
-_wait_for_user_confirmation("4.2")
+_wait_for_user_confirmation("3.2")
 
 # If you want to check whether two trees are equal (both nodes have the same value, balance value and all the children are the same),
 # you can use the is_equal_including_subtrees() method.
-print("4.3 root.is_equal_including_subtrees(root_copy) =",
+print("3.3 root.is_equal_including_subtrees(root_copy) =",
       root.is_equal_including_subtrees(root_copy))
 # The result is: False. While the root was not changed, it's right child was modified, therefore the two trees are no longer equal.
-_wait_for_user_confirmation("4.3")
+_wait_for_user_confirmation("3.3")
 
 
 ################
-# 5. Traversal #
+# 4. Traversal #
 ################
 
 # You can traverse the tree manually by calling 'get_left_child()', 'get_right_child()' or 'get_parent()',
@@ -114,5 +111,5 @@ nodes_in_preorder = root.preorder_traverse()
 nodes_in_inorder = root.inorder_traverse()
 nodes_in_postorder = root.postorder_traverse()
 
-print("5.1 Nodes in preorder:")
+print("4.1 Nodes in preorder:")
 print(nodes_in_preorder)
--- checker/example_usage_priority_queue.py
+++ checker/example_usage_priority_queue.py
@@ -5,22 +5,19 @@ To avoid the output from being overwhelming, after each console output you have 
 For that purpose the '_wait_for_user_confirmation()' function is used here. Please ignore it.
 """
 
-def _wait_for_user_confirmation(output_text):
-    input(
-        f"Please press 'Enter' to continue to continue past output {output_text}")
-
-##############
-# 1. Imports #
-##############
-
 # Import the PriorityQueueNode class
 from datastructures.priorityqueue import PriorityQueueNode
 # Import the json module for parsing the JSON string into a dictionary
 import json
 
 
+def _wait_for_user_confirmation(output_text):
+    input(
+        f"Please press 'Enter' to continue to continue past output {output_text}")
+
+
 ####################################
-# 2. Creating an PriorityQueueNode #
+# 1. Creating an PriorityQueueNode #
 ####################################
 
 # This is an example for an input JSON, where there is just one node (hence balancing factor 0).
@@ -35,12 +32,12 @@ root = PriorityQueueNode.from_dict(example_dictionary)
 
 # If Graphviz is installed, the priority queue can be visualized by calling 'display_priority_queue_image(title)'
 root.display_priority_queue_image(
-    "2.1 This is the initial priority queue\ncontaining just '5'.")
-_wait_for_user_confirmation("2.1")
+    "1.1 This is the initial priority queue\ncontaining just '5'.")
+_wait_for_user_confirmation("1.1")
 
 
 ###################################
-# 3. Modifying the priority queue #
+# 2. Modifying the priority queue #
 ###################################
 
 # We are just going to assume, that we have a min-priority queue (parent must be smaller or equal to its children)
@@ -49,8 +46,8 @@ new_left_child = PriorityQueueNode(value=10)
 root.set_left_child(new_left_child)
 
 root.display_priority_queue_image(
-    "3.1 Now a left child was added.")
-_wait_for_user_confirmation("3.1")
+    "2.1 Now a left child was added.")
+_wait_for_user_confirmation("2.1")
 
 # Now two nodes are added at the correct positions.
 # One to the left of the root and one to the left of the root's right child.
@@ -61,12 +58,12 @@ new_left_left_child = PriorityQueueNode(value=15)
 root.get_left_child().set_left_child(new_left_left_child)
 
 root.display_priority_queue_image(
-    "3.2 Two nodes were added.")
-_wait_for_user_confirmation("3.2")
+    "2.2 Two nodes were added.")
+_wait_for_user_confirmation("2.2")
 
 
 #################################################
-# 4. Creating a copy and comparing for equality #
+# 3. Creating a copy and comparing for equality #
 #################################################
 
 # Now create a copy, so it can be modified without affecting the original. Use the deep_copy() method for that.
@@ -76,26 +73,26 @@ root_copy = root.deep_copy()
 root_copy.get_right_child().set_value(8)
 
 # One can use the regular comparison operators to check if two nodes are equal (have the same value)
-print("4.1 root.get_right_child() == root_copy.get_right_child() =",
+print("3.1 root.get_right_child() == root_copy.get_right_child() =",
       root.get_right_child() == root_copy.get_right_child())
 # The result is: False. The node was modified and is therefore not equal to the original.
-_wait_for_user_confirmation("4.1")
+_wait_for_user_confirmation("3.1")
 
-print("4.2 root == root_copy =", root == root_copy)
+print("3.2 root == root_copy =", root == root_copy)
 # The result is: True. While the right child of the root was modified, the root itself was not changed.
 # So the roots are still equal.
-_wait_for_user_confirmation("4.2")
+_wait_for_user_confirmation("3.2")
 
 # If you want to check whether two trees are equal (both nodes have the same value and all the children are the same),
 # you can use the is_equal_including_subtrees() method.
-print("4.3 root.is_equal_including_subtrees(root_copy) =",
+print("3.3 root.is_equal_including_subtrees(root_copy) =",
       root.is_equal_including_subtrees(root_copy))
 # The result is: False. While the root was not changed, it's right child was modified, therefore the two trees are no longer equal.
-_wait_for_user_confirmation("4.3")
+_wait_for_user_confirmation("3.3")
 
 
 ################
-# 5. Traversal #
+# 4. Traversal #
 ################
 
 # You can traverse the tree manually by calling 'get_left_child()', 'get_right_child()' or 'get_parent()',
@@ -105,5 +102,5 @@ nodes_in_preorder = root.preorder_traverse()
 nodes_in_inorder = root.inorder_traverse()
 nodes_in_postorder = root.postorder_traverse()
 
-print("5.1 Nodes in preorder:")
+print("4.1 Nodes in preorder:")
 print(nodes_in_preorder)
